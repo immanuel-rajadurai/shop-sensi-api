e49baf3fdce4fbc9719c5aa8b16572d3b7d43387
Immanuel Rajadurai <immanuel.rajadurai@kcl.ac.uk>
2024-04-03 17:17:24 +0100

    ammend to suit hheroku postres settings

--- sensi/settings.py
+++ sensi/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = 'django-insecure-eolgt!hs@#w75!2s@lh_t!noz@)ravl3x9jwf8w(e5x^wj6rgh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = ['shop-sensi-api.herokuapp.com']
 
 # Application definition
 
@@ -75,10 +76,21 @@ WSGI_APPLICATION = 'sensi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dc4auqole59au7',
+        'USER': 'vbdxehrcscfdiy',
+        'PASSWORD':'898fcb505f3d1e653d6e3bc5718f70cc4250270ef8168d389e41adda22f900df',
+        'HOST':'ec2-34-254-138-204.eu-west-1.compute.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
