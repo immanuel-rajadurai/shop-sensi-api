67136e9517843d8d760f733bfb44068128ca3ef9
Immanuel Rajadurai <immanuel.rajadurai@kcl.ac.uk>
2024-04-03 18:07:49 +0100

    add Answerset to admin

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Product, QuestionSet
+from .models import Product, QuestionSet, AnswerSet
 
 
 # Register your models here.
 
-
 admin.site.register(Product)
 admin.site.register(QuestionSet)
+admin.site.register(AnswerSet)
