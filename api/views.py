c1040d81ab862058d4a25e628d2700ac39a734c6
Immanuel Rajadurai <immanuel.rajadurai@kcl.ac.uk>
2024-04-04 23:57:44 +0100

    add  csrf protection

--- api/views.py
+++ api/views.py
@@ -10,7 +10,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from .question_generator import QuestionGenerator
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 @api_view(['GET'])
 def get_questions_list_for_product(request, product_title):
@@ -40,7 +40,7 @@ def get_questions_list_for_product(request, product_title):
     except Exception as e:
         return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
  
-@csrf_exempt
+@csrf_protect
 @api_view(['POST'])
 def add_product(request):
     try:
